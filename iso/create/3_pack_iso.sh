688599dfb44a647bd158ef9344749cc36b8b08f1
Martin <none@easy-vdr.de>
2019-12-19 18:42:16 +0100

    ISO-Build

--- iso/create/3_pack_iso.sh
+++ iso/create/3_pack_iso.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 DIR=/home/easyvdr-iso/ISO/
-IMAGE=$DIR/ea5-prealpha-005.iso
+IMAGE=$DIR/ea5-prealpha-006.iso
 BUILD=$DIR/cd-image/
-
-
-#sudo mount -o loop $DIR/org/*.iso $DIR/loop
-#sudo rsync -av $DIR/loop/ $DIR/cd-image
-
-
 
 
 cd $DIR/cd-image
 chmod 666 $DIR/cd-image/install/filesystem.size
 sudo du -sx --block-size=1 ./ | cut -f1 > $DIR/cd-image/install/filesystem.size
+
 #
 #mkdir -p /opt/indices /opt/apt-ftparchive
 #cd /opt/indices/
@@ -24,27 +19,14 @@ sudo du -sx --block-size=1 ./ | cut -f1 > $DIR/cd-image/install/filesystem.size
 
 
 
-sudo mkisofs -r -V "easyVDR Server-ISO 001" \
-            -cache-inodes \
-            -J -l -b isolinux/isolinux.bin \
-            -c isolinux/boot.cat -no-emul-boot \
-            -boot-load-size 4 -boot-info-table \
-            -o $IMAGE $BUILD
-
-
-isohybrid $IMAGE
-#isohybrid -u fertiges-isoimage.iso
+#sudo xorriso -as mkisofs -no-emul-boot -boot-load-size 4 -boot-info-table -iso-level 4 -b isolinux/isolinux.bin -c isolinux/boot.cat -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -o $IMAGE $BUILD
+#sudo xorriso -as mkisofs -no-emul-boot -boot-load-size 4 -boot-info-table -iso-level 4 -b isolinux/isolinux.bin -c isolinux/boot.cat -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin -partition_offset 16   -o $IMAGE $BUILD
+sudo xorriso -as mkisofs -no-emul-boot -boot-load-size 4 -boot-info-table -iso-level 4 -b isolinux/isolinux.bin -c isolinux/boot.cat -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -isohybrid-gpt-basdat /usr/lib/ISOLINUX/isohdpfx.bin -partition_offset 16   -o $IMAGE $BUILD
 
 
 
 
-
-#cd ..
-#sudo xorriso -as mkisofs -no-emul-boot -boot-load-size 4 -boot-info-table -iso-level 4 -b isolinux/isolinux.bin -c isolinux/boot.cat -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -o ../fertiges-isoimage.iso .
-#cd ..
-#
-#ISO="easyvdr-4.0.0-64-lubuntu-prealpha010.iso"
-#mv fertiges-isoimage.iso $ISO
-#zsyncmake $ISO
-#md5sum $ISO >md5.txt
+#isohybrid -u $IMAGE
+#zsyncmake $IMAGE
+#md5sum $IMAGE >md5.txt
 
