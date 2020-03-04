383b1168e091d0afeb90d48d525d79be8a8bb328
Martin <none@easy-vdr.de>
2020-03-04 08:30:33 +0100

    Isohybrid for Image...
    Additional isohybrid command used

--- iso/create/3_pack_iso.sh
+++ iso/create/3_pack_iso.sh
@@ -22,11 +22,9 @@ sudo du -sx --block-size=1 ./ | cut -f1 > $DIR/cd-image/install/filesystem.size
 #sudo xorriso -as mkisofs -no-emul-boot -boot-load-size 4 -boot-info-table -iso-level 4 -b isolinux/isolinux.bin -c isolinux/boot.cat -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -o $IMAGE $BUILD
 #sudo xorriso -as mkisofs -no-emul-boot -boot-load-size 4 -boot-info-table -iso-level 4 -b isolinux/isolinux.bin -c isolinux/boot.cat -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin -partition_offset 16   -o $IMAGE $BUILD
 sudo xorriso -as mkisofs -no-emul-boot -boot-load-size 4 -boot-info-table -iso-level 4 -b isolinux/isolinux.bin -c isolinux/boot.cat -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -isohybrid-gpt-basdat /usr/lib/ISOLINUX/isohdpfx.bin -partition_offset 16   -o $IMAGE $BUILD
+isohybrid -u $IMAGE
 
 
-
-
-#isohybrid -u $IMAGE
 #zsyncmake $IMAGE
 #md5sum $IMAGE >md5.txt
 
